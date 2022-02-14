fb5c9d5bed53985c9b0a0463eb7285a211455984
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-14 14:31:42 +0000

    learned methods for objects and dictionaries

---
+++ objects.js
@@ -0,0 +1,40 @@
+
+//Created dictionary/object
+var movie = {
+    title: "It's a Wonderful Life",
+    year: 1946,
+    language: "Spanish"
+};
+
+//Show all elements
+console.log(movie)
+console.log()
+
+//Select a key
+console.log(movie['title'])
+console.log(movie.title)
+console.log()
+
+//Add a key pair
+movie.cast = ["James Streart", "Donna Reed"]
+console.log(movie)
+console.log()
+
+//Update value 
+movie.language = "English"
+console.log(movie)
+
+//Selection option
+//string used as '-' in name
+movie["subtitle-language"] = "German"
+console.log(movie)
+
+//delete an object
+delete movie.year
+
+//iterate through a dictionary's keys
+for (var key in movie){
+    console.log(key)
+}
+
+//
