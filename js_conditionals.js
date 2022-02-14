b264af0128a7e4b657172ba97cbf459f0790fc17
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-14 11:43:18 +0000

    Second upload

---
+++ js_conditionals.js
@@ -0,0 +1,17 @@
+// conditional statement syntax
+if  (1 > 0) {
+    var message = "1 is greater than 0, duh!"
+} else if (1 == 0) {
+    var message = " 1 = 0? Reality is literally broken, the guy from 'Empire Broadwalk' was right!"
+} else {
+    var message = "1 is less than 0? Wtf, how does that work?"
+}
+
+// not statement 
+console.log("Not statement", (1 + 1 != 3))
+
+// and statement
+console.log("And statement",(1 + 1 == 2) && (1 + 1 == 4))
+
+// or statement
+console.log("Or statement", (1 + 1 == 2) || (1 + 1 == 4))
---
+++ js_fundamentals.js
@@ -0,0 +1,63 @@
+//Print to console
+console.log("Hello World\n");
+
+//Print a var. Use the below format to print var name with var
+var disney_char = "Mickey";
+console.log('disney_char:', disney_char)
+
+// JS needs vars to be declared when they are created.
+var greeting = "hi planet";
+greeting = "hElLo wOrLD"
+console.log("greeting:", greeting);
+
+console.log(" ")
+
+// Print type
+console.log(typeof "hello");
+console.log(typeof 1);
+
+console.log(" ")
+
+// Apply a function 
+console.log("Unmodified string " + "uppercase string");
+console.log("Modified string " + "uppercase string\n".toUpperCase());
+
+// coercing strings
+console.log(3 + " lemons")
+
+console.log( 6 + 6 + " rings\n")
+
+
+// JS has one standard number type
+console.log(typeof 1)
+console.log(typeof 0.5)
+
+console.log(" ")
+
+// Use backticks '`' to make f strings
+var firstNam = "Adam";
+console.log(`This is a backtick string: Hello ${firstNam}.` )
+
+// Concatenation
+console.log("This is a concatenated string: Hello " + firstNam + ".\n")
+
+
+//undefined variable
+var myVariable;
+console.log(myVariable)
+
+//Nan variable
+console.log(myVariable + 1)
+
+console.log(" ")
+
+//loose equality
+console.log("loose equality:", 2 == "2")
+
+//strict equality
+console.log("strict equality", 2 === "2")
+
+console.log(" ")
+
+
+
